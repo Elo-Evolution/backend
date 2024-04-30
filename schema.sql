769ebeb49d20ed9878511d33764ab92ea8759b3c
Maxwell Mendenhall <97015178+MaxwellMendenhall@users.noreply.github.com>
2024-04-29 23:16:13 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,15 +1,6 @@
 DROP TABLE IF EXISTS user_stats;
 DROP TABLE IF EXISTS user;
 
-CREATE TABLE user_stats
-(
-    id         INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id    TEXT UNIQUE NOT NULL,
-    mse_attack REAL,
-    mse_defend REAL,
-    FOREIGN KEY (user_id) REFERENCES user (id)
-);
-
 CREATE TABLE user
 (
     id            INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,4 +8,13 @@ CREATE TABLE user
     password_hash TEXT        NOT NULL,
     r6_user_id    TEXT,
     created_at    DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE user_stats
+(
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id    TEXT UNIQUE NOT NULL,
+    mse_attack REAL,
+    mse_defend REAL,
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
